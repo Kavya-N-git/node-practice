7c4fea355917d933b972d5d12fd189a4e02c5c07
Kavya-N-git <nkavya2017@gmail.com>
2022-12-15 14:13:03 +0530

    added

---
+++ index.js
@@ -0,0 +1,6 @@
+//before node to run js it was necessary to use script tag inside html but in node its possible to run with html
+
+console.log("hello world , welcome to node js learning session");
+
+//to run js file in node js environment syntax followed is 
+// node <file name> eg: node index.js
